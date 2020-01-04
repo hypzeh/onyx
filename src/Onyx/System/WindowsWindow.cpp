dfaf9582f3a156c2781ef28f8ea2f080b70fde06
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2020-01-04 00:41:41 +0000

    Added constructor

--- src/Onyx/System/WindowsWindow.cpp
+++ src/Onyx/System/WindowsWindow.cpp
@@ -5,6 +5,11 @@ namespace Onyx::System
 {
 	std::unique_ptr<Window> Window::Create(const WindowProperties& properties)
 	{
-		return std::make_unique<WindowsWindow>();
+		return std::make_unique<WindowsWindow>(properties);
+	}
+
+	WindowsWindow::WindowsWindow(const WindowProperties& properties)
+	{
+
 	}
 }
--- src/Onyx/System/WindowsWindow.h
+++ src/Onyx/System/WindowsWindow.h
@@ -5,6 +5,7 @@ namespace Onyx::System
 {
 	class WindowsWindow : public Window
 	{
-
+	public:
+		WindowsWindow(const WindowProperties& properties);
 	};
 }
